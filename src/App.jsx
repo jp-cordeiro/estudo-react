fd43a6c432f86c8037a3ffe7cf7cb5df137ed29d
JP <jpc.araujo@hotmail.com>
2021-01-30 16:21:14 -0300

    add App styles file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './app.scss';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Products from './pages/Products';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Products}></Route>
-        </Switch>
-      </BrowserRouter>
+    <div id="app">
+      <Header />
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Products}></Route>
+          </Switch>
+        </BrowserRouter>
+      </main>
+      <Footer />
     </div>
   );
 }
