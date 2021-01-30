58548522ded7e14552e7532bc445bbf7cc61f42a
JP <jpc.araujo@hotmail.com>
2021-01-30 17:26:25 -0300

    use Head component

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Head from '../../components/Head';
 
 import './products.scss';
 
 export default function Products() {
-  return <div></div>;
+  return (
+    <div className="anime-left">
+      <Head title="Ranek | Produtos" />
+    </div>
+  );
 }
