269b124757675e0c3d237f4b1ee0b8c23be5a858
JP <jpc.araujo@hotmail.com>
2021-01-30 16:08:43 -0300

    add products component and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Products from './pages/Products';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Products}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
