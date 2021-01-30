b05206a779a36455283f823b77e47b0ef1c8a708
JP <jpc.araujo@hotmail.com>
2021-01-30 17:26:12 -0300

    add Contact behavior

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Head from '../../components/Head';
+
+import image from '../../images/contato.jpg';
 
 import './contact.scss';
 
 export default function Contact() {
-  return <div></div>;
+  return (
+    <section className='anime-left'>
+      <Head title='Ranek | Contato'/>
+      <img src={image} alt="Máquina de escrever" />
+      <div>
+        <h1>Entre em contato</h1>
+        <ul>
+          <li>teste@teste.com</li>
+          <li>(00) 0000-0000</li>
+          <li>Rua Teste, 000</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
