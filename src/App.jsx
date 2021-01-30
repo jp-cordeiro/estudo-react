74d18fd138aa3920f8414a9f721b3babf544b57c
JP <jpc.araujo@hotmail.com>
2021-01-30 16:33:06 -0300

    refactoring App

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Products from './pages/Products';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div id="app">
-      <Header />
       <main>
         <BrowserRouter>
+          <Header />
           <Switch>
             <Route path="/" component={Products}></Route>
+            <Route path="/contato" component={Contact}></Route>
           </Switch>
+          <Footer />
         </BrowserRouter>
       </main>
-      <Footer />
     </div>
   );
 }
